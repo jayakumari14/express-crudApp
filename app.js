a2aa8060615d6437bff85b4224e884291ec5cde7
jayak <kumarijaya127@gmail.com>
2024-04-24 22:21:41 +0530

    added views and public folder

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("welcome home");
+});
+
+app.listen(3000);
