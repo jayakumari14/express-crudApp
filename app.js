508e51ed82031a34d30c470cb95705831abb1758
jayak <kumarijaya127@gmail.com>
2024-04-25 22:41:04 +0530

    add Update and Read

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ app.get("/read", async (req, res) => {
   res.render("read", { users: allUser });
 });
 
+app.get("/edit/:userid", async (req, res) => {
+  let user = await userModel.findOne({ _id: req.params.userid });
+  res.render("edit", { user });
+});
+
+app.post("/update/:userid", async (req, res) => {
+  let { name, email, image } = req.body;
+  let user = await userModel.findOneAndUpdate(
+    { _id: req.params.userid },
+    { name, email, image },
+    { new: true }
+  );
+  res.redirect("/read");
+});
+
 app.post("/create", async (req, res) => {
   let { name, email, image } = req.body;
   let createdUser = await userModel.create({
